bb1199fa1b4a736be31362f88aca8cfd4a6f6655
Daniela <139877343+Danykia93@users.noreply.github.com>
2023-07-18 23:12:30 -0300

    Update convertendo_tipos.py

--- 00 - Fundamentos/convertendo_tipos.py
+++ 00 - Fundamentos/convertendo_tipos.py
@@ -1,12 +1,17 @@
-print(int(1.97348728))
-print(int("10"))
-print(float("10.10"))
-print(float(100))
+### transformando numero inteeiro para descimal, de decimal para inteiro.
 
-valor = 10
+print(int(5.9784523)) # imprime apenas um valor inteiro #1
+print(int("8")) # imprime apenas o valor inteiro com string #10
+print(float("8.8")) # imprime valor decimal #10.1
+print(float(70)) # imprime o valor com casa decimal #100.1
+
+valor = 30
 valor_str = str(valor)
-print(type(valor))
-print(type(valor_str))
+print(type(valor)) # classe "int"
+print(type(valor_str)) # classe 'str"
 
-print(100 / 2)
-print(100 // 2)
+print(300 / 2) # ao utilizar um "/" o numero fical com casa decimal #150.0
+print(15 * 2) # 30
+print((2 * 3) / 1) # como tem uma barra fica dividindo por 1, entao fica #
+print((2 * 3) // 1)
+print(100 // 2) # ao imrpimir com duas "//" o numero fica inteiro #50"
