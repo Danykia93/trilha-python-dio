f5227bff5f3689fdd2cebd27e4aecbfe2c6fcaeb
Daniela <139877343+Danykia93@users.noreply.github.com>
2023-07-20 18:23:52 -0300

    Update desafio.py

--- 01 - Estrutura de dados/desafio.py
+++ 01 - Estrutura de dados/desafio.py
@@ -4,8 +4,8 @@ import textwrap
 def menu():
     menu = """\n
     ================ MENU ================
-    [d]\tDepositar
-    [s]\tSacar
+    [d]Depositar
+    [s]Sacar
     [e]\tExtrato
     [nc]\tNova conta
     [lc]\tListar contas
@@ -21,7 +21,7 @@ def depositar(saldo, valor, extrato, /):
         extrato += f"Depósito:\tR$ {valor:.2f}\n"
         print("\n=== Depósito realizado com sucesso! ===")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n ###Operação falhou! O valor informado é inválido. ###")
 
     return saldo, extrato
 
@@ -32,22 +32,22 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n### Operação falhou! Você não tem saldo suficiente. ###")
 
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n### Operação falhou! O valor do saque excede o limite. ###")
 
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n### Operação falhou! Número máximo de saques excedido. ###")
 
     elif valor > 0:
         saldo -= valor
-        extrato += f"Saque:\t\tR$ {valor:.2f}\n"
+        extrato += f"Saque:R$ {valor:.2f}\n"
         numero_saques += 1
         print("\n=== Saque realizado com sucesso! ===")
 
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n### Operação falhou! O valor informado é inválido. ###")
 
     return saldo, extrato
 
@@ -64,7 +64,7 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n### Já existe usuário com esse CPF! ###")
         return
 
     nome = input("Informe o nome completo: ")
